5ef5581b0318087824f454334da882c61d8e5964
kaifcodec <kaifcodec@gmail.com>
2025-08-03 22:39:11 +0530

    .js include

--- ssl_unpinning.js
+++ ssl_unpinning.js
@@ -1,67 +1,92 @@
-// A simple Frida script for SSL unpinning.
-// It hooks various common SSL/TLS verification functions.
-
+/*
+ * Universal Android SSL Pinning Bypass with Frida
+ * This script attempts to bypass SSL pinning in a variety of common Android libraries.
+ */
 if (Java.available) {
     Java.perform(function() {
-        console.log("SSL Unpinning script loaded.");
+        console.log("-> SSL unpinning script loaded.");
 
-        // -- Android 7.0+ (Nougat) --
+        var CertificateFactory = Java.use("java.security.cert.CertificateFactory");
+        var FileInputStream = Java.use("java.io.FileInputStream");
+        var BufferedInputStream = Java.use("java.io.BufferedInputStream");
+        var X509Certificate = Java.use("java.security.cert.X509Certificate");
+        var KeyStore = Java.use("java.security.KeyStore");
+        var TrustManagerFactory = Java.use('javax.net.ssl.TrustManagerFactory');
+        var SSLContext = Java.use('javax.net.ssl.SSLContext');
+
+        // -- Certificate pinning bypass --
         try {
             var TrustManagerImpl = Java.use('com.android.org.conscrypt.TrustManagerImpl');
             TrustManagerImpl.checkTrusted.implementation = function(chain, authType, host) {
-                console.log('TrustManagerImpl.checkTrusted() called');
+                console.log("[+] TrustManagerImpl.checkTrusted() called. Bypassing pinning for: " + host);
                 return;
             };
-        } catch (err) {
-            console.log('[-] TrustManagerImpl.checkTrusted() not found or hook failed.');
+        } catch (e) {
+            console.log("[-] TrustManagerImpl.checkTrusted() not found or failed to hook.");
         }
 
-        // -- Square OkHttp3 --
+        // -- OkHttp3 --
         try {
             var CertificatePinner = Java.use('okhttp3.CertificatePinner');
-            CertificatePinner.check.overload('java.lang.String', 'java.util.List').implementation = function(hostname, certificates) {
-                console.log('CertificatePinner.check() called. Bypassing SSL pinning for ' + hostname);
+            CertificatePinner.check.overload('java.lang.String', 'java.util.List').implementation = function() {
+                console.log("[+] OkHttp3 CertificatePinner.check() called. Bypassing pinning.");
                 return;
             };
-        } catch (err) {
-            console.log('[-] OkHttp3 CertificatePinner hook failed.');
+        } catch (e) {
+            console.log("[-] OkHttp3 CertificatePinner.check() hook failed.");
         }
-        
+
         // -- TrustKit --
         try {
             var TrustKit = Java.use('com.datatheorem.android.trustkit.pinning.OkHttp3SslPinningInterceptor');
             TrustKit.intercept.implementation = function(chain) {
-                console.log('TrustKit.intercept() called. Bypassing SSL pinning.');
+                console.log("[+] TrustKit.intercept() called. Bypassing pinning.");
                 return this.intercept.call(this, chain);
             };
-        } catch (err) {
-            console.log('[-] TrustKit hook failed.');
+        } catch (e) {
+            console.log("[-] TrustKit.intercept() hook failed.");
         }
-        
-        // -- Other common SSL pinning libraries --
+
+        // -- WebView --
         try {
             var WebView = Java.use('android.webkit.WebView');
             WebView.setWebViewClient.implementation = function(client) {
-                console.log('WebView.setWebViewClient() called. Bypassing SSL pinning...');
+                console.log("[+] WebView.setWebViewClient() called. Bypassing pinning.");
                 this.setWebViewClient.call(this, client);
             };
-        } catch (err) {
-            console.log('[-] WebView hook failed.');
+        } catch (e) {
+            console.log("[-] WebView.setWebViewClient() hook failed.");
         }
 
-        // -- Alternative for SSLPinning --
+        // -- Custom TrustManager bypass --
         try {
-            var sslPinning = Java.use('net.sqlcipher.database.SQLiteDatabase');
-            sslPinning.enableSSLPinning.implementation = function(arg) {
-                console.log('SSLPinning.enableSSLPinning() called. Bypassing SSL pinning.');
-                return true;
+            var X509TrustManager = Java.use('javax.net.ssl.X509TrustManager');
+            var TrustManager = Java.registerClass({
+                name: 'com.r0ck.TrustManager',
+                implements: [X509TrustManager],
+                methods: {
+                    checkClientTrusted: function(chain, authType) {},
+                    checkServerTrusted: function(chain, authType) {},
+                    getAcceptedIssuers: function() {
+                        return [];
+                    }
+                }
+            });
+
+            TrustManagerFactory.getTrustManagers.implementation = function() {
+                console.log("[+] TrustManagerFactory.getTrustManagers() called. Using custom TrustManager.");
+                return [TrustManager.$new()];
             };
-        } catch(err) {
-            console.log('[-] net.sqlcipher hook failed.');
+
+            SSLContext.init.implementation = function(kmf, tmf, sr) {
+                console.log("[+] SSLContext.init() called. Using custom TrustManager.");
+                this.init.call(this, kmf, [TrustManager.$new()], sr);
+            };
+
+        } catch (e) {
+            console.log("[-] TrustManager bypass failed.");
         }
-        
-        console.log("SSL Unpinning script finished loading.");
+
+        console.log("-> SSL unpinning script finished loading.");
     });
-} else {
-    console.log("Java not available. Frida Gadget may have loaded too early.");
 }
