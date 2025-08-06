ea3059e1abf0e6c73475428423ba01a1d488e45b
kaifcodec <kaifcodec@gmail.com>
2025-08-06 10:16:33 +0530

    none

--- ssl_unpinning.js
+++ ssl_unpinning.js
@@ -1,70 +1,119 @@
-Java.perform(function () {
-    console.log("Universal SSL unpinning script loaded");
+'use strict'
 
-    // OkHTTPv3 & v4 bypass
+function disableTigon() {
     try {
-        var okhttp3_TrustManager = Java.use("okhttp3.CertificatePinner");
-        okhttp3_TrustManager.check.overload("java.lang.String", "java.util.List").implementation = function () {
-            console.log("[+] Bypassed OkHTTPv3/4 CertificatePinner");
-            return;
-        };
-    } catch (err) {
-        console.log("[-] OkHTTP CertificatePinner not found");
+        Java.perform(() => {
+            let IGTigonConfig = Java.use("com.instagram.service.tigon.configs.IGTigonConfig");
+            IGTigonConfig.$init.implementation = function() {
+                this.$init();
+                this.isTigonEnabled.value = false;
+                this.isMnsEnabled.value = false;
+                logger("[*][+] Disable tigon")
+            };
+        });
+        logger("[*][+] Hooked to IGTigonConfig.$init")
+    } catch (e) {
+        logger("[*][-] Failed to IGTigonConfig.$init")
+    }
+}
+
+function hook_proxygen_SSLVerification(library) {
+    const verifyFunction = library.enumerateExports().find(el => el.name.includes("verifyWithMetrics"));
+
+    if (!verifyFunction) {
+        logger(`[*][-] Failed to find verifyWithMetrics function`);
+        return;
     }
 
-    // TrustManagerImpl (Android default)
     try {
-        var TrustManagerImpl = Java.use("com.android.org.conscrypt.TrustManagerImpl");
-        TrustManagerImpl.verifyChain.implementation = function (untrustedChain, trustAnchorChain, host, tlsContext) {
-            console.log("[+] Bypassed TrustManagerImpl.verifyChain");
-            return untrustedChain;
-        };
-    } catch (err) {
-        console.log("[-] TrustManagerImpl not found");
-    }
-
-    // WebViewClient onReceivedSslError
-    try {
-        var WebViewClient = Java.use("android.webkit.WebViewClient");
-        WebViewClient.onReceivedSslError.implementation = function (webview, handler, error) {
-            console.log("[+] Bypassed WebViewClient onReceivedSslError");
-            handler.proceed();
-        };
-    } catch (err) {
-        console.log("[-] WebViewClient not found");
-    }
-
-    // Apache Harmony
-    try {
-        var sslSession = Java.use("org.apache.harmony.xnet.provider.jsse.OpenSSLSocketImpl");
-        sslSession.startHandshake.implementation = function () {
-            console.log("[+] Bypassed Apache Harmony SSL handshake");
-            // Don't call the real handshake
-        };
-    } catch (err) {
-        console.log("[-] Apache Harmony SSL socket not found");
-    }
-
-    // TrustAllManager fallback
-    try {
-        var X509TrustManager = Java.use("javax.net.ssl.X509TrustManager");
-        var TrustManager = Java.registerClass({
-            name: "org.kaif.CustomTrustManager",
-            implements: [X509TrustManager],
-            methods: {
-                checkClientTrusted: function (chain, authType) {},
-                checkServerTrusted: function (chain, authType) {},
-                getAcceptedIssuers: function () { return []; }
+        Interceptor.attach(verifyFunction.address, {
+            onLeave: function(retvalue) {
+                retvalue.replace(1);
             }
         });
-        var SSLContext = Java.use("javax.net.ssl.SSLContext");
-        SSLContext.init.overload("[Ljavax.net.ssl.KeyManager;", "[Ljavax.net.ssl.TrustManager;", "java.security.SecureRandom")
-            .implementation = function (keyManager, trustManager, secureRandom) {
-                console.log("[+] Injected TrustAllManager into SSLContext");
-                this.init(keyManager, [TrustManager.$new()], secureRandom);
-            };
+        logger(`[*][+] Hooked function: ${verifyFunction.name}`);
     } catch (err) {
-        console.log("[-] Could not patch SSLContext");
+        logger(`[*][-] Failed to hook function: ${verifyFunction.name}`);
+        logger(err.toString())
     }
+}
 
+function waitForModule(moduleName) {
+    return new Promise(resolve => {
+        const interval = setInterval(() => {
+            const module = Process.findModuleByName(moduleName);
+            if (module != null) {
+                clearInterval(interval);
+                resolve(module);
+            }
+        }, 300);
+    });
+}
+
+function logger(message) {
+    console.log(message);
+    Java.perform(function() {
+        var Log = Java.use("android.util.Log");
+        Log.v("INSTAGRAM_SSL_PINNING_BYPASS", message);
+    });
+}
+
+
+logger("[*][*] Waiting for libstartup.so...");
+waitForModule("libstartup.so").then((lib) => {
+    logger(`[*][+] Found libstartup.so at: ${lib.base}`)
+    hook_proxygen_SSLVerification(lib);
 });
+
+disableTigon();
+//Universal Android SSL Pinning Bypass #2
+Java.perform(function() {
+    try {
+        var array_list = Java.use("java.util.ArrayList");
+        var ApiClient = Java.use('com.android.org.conscrypt.TrustManagerImpl');
+        if (ApiClient.checkTrustedRecursive) {
+            logger("[*][+] Hooked checkTrustedRecursive")
+            ApiClient.checkTrustedRecursive.implementation = function(a1, a2, a3, a4, a5, a6) {
+                var k = array_list.$new();
+                return k;
+            }
+        } else {
+            logger("[*][-] checkTrustedRecursive not Found")
+        }
+    } catch (e) {
+        logger("[*][-] Failed to hook checkTrustedRecursive")
+    }
+});
+
+Java.perform(function() {
+    try {
+        const x509TrustManager = Java.use("javax.net.ssl.X509TrustManager");
+        const sSLContext = Java.use("javax.net.ssl.SSLContext");
+        const TrustManager = Java.registerClass({
+            implements: [x509TrustManager],
+            methods: {
+                checkClientTrusted(chain, authType) {
+                },
+                checkServerTrusted(chain, authType) {
+                },
+                getAcceptedIssuers() {
+                    return [];
+                },
+            },
+            name: "com.leftenter.instagram",
+        });
+        const TrustManagers = [TrustManager.$new()];
+        const SSLContextInit = sSLContext.init.overload(
+            "[Ljavax.net.ssl.KeyManager;", "[Ljavax.net.ssl.TrustManager;", "java.security.SecureRandom");
+        SSLContextInit.implementation = function(keyManager, trustManager, secureRandom) {
+            SSLContextInit.call(this, keyManager, TrustManagers, secureRandom);
+        };
+        logger("[*][+] Hooked SSLContextInit")
+    } catch (e) {
+        logger("[*][-] Failed to hook SSLContextInit")
+    }
+})
+
+
+
+
